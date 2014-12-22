6242e4fb3446fe219a3e1ffe2088f26be63dfbf2
Günther Haslbeck <guentherhaslbeck@gmail.com>
2014-12-22 12:18:23 +0100

    first strike

---
+++ servlet2/src/main/java/devmanuals/servlet/ServletRequestExample.java
@@ -0,0 +1,187 @@
+package main.java.devmanuals.servlet;
+
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.io.File;
+import java.sql.*;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lyncode.jtwig.JtwigModelMap;
+import com.lyncode.jtwig.resource.FileJtwigResource;
+import com.lyncode.jtwig.JtwigTemplate;
+import com.lyncode.jtwig.configuration.JtwigConfiguration;
+import com.lyncode.jtwig.content.api.Renderable;
+import com.lyncode.jtwig.render.RenderContext;
+import com.lyncode.jtwig.resource.ClasspathJtwigResource;
+
+public class ServletRequestExample extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		doAll(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		doAll(request, response);
+	}
+
+	public HashMap resultSetToHashMap(ResultSet rs) throws SQLException {
+		ResultSetMetaData md = rs.getMetaData();
+		int columns = md.getColumnCount();
+		HashMap row = new HashMap();
+		while (rs.next()) {
+			for (int i = 1; i <= columns; i++) {
+				row.put(md.getColumnName(i), rs.getObject(i));
+			}
+		}
+		return row;
+	}
+
+	protected void doAll(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<br>getAuthType()- " + request.getAuthType());
+
+		String id = request.getParameter("id");
+
+		try {
+			JtwigConfiguration configuration = new JtwigConfiguration();
+
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			Renderable compiled = new JtwigTemplate(
+					new File(
+							"/var/lib/tomcat6/webapps/servletexample/WEB-INF/classes/template.twig"),
+					configuration).compile();
+			out.println("--DB-TEST--");
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("test", "test");
+			map.put("name", "guenther");
+			map.put("id", id);
+
+			// JDBC driver name and database URL
+			String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+			String DB_URL = "jdbc:mysql://localhost/usr_web30_1";
+
+			// Database credentials
+			String USER = "root";
+			String PASS = "123456";
+
+			Connection conn = null;
+			Statement stmt = null;
+			try {
+				// STEP 2: Register JDBC driver
+				Class.forName("com.mysql.jdbc.Driver");
+
+				// STEP 3: Open a connection
+				conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+				// STEP 4: Execute a query
+				stmt = conn.createStatement();
+				String sql;
+				sql = "SELECT username FROM user limit 0,10";
+				ResultSet rs = stmt.executeQuery(sql);
+
+				HashMap hm1 = new HashMap();
+				ResultSetMetaData md = rs.getMetaData();
+				int columns = md.getColumnCount();
+				int x = 1;
+				while (rs.next()) {
+					x++;
+					HashMap row = new HashMap();
+					for (int i = 1; i <= columns; i++) {
+						row.put(md.getColumnName(i), rs.getObject(i));
+					}
+					hm1.put("line" + x, row);
+				}
+
+				map.put("users", hm1);
+				// map.put("users", resultSetToHashMap(rs));
+
+				// STEP 5: Extract data from result set
+				/*
+				 * while(rs.next()){ //Retrieve by column name
+				 * out.println("o:"+rs.getString("username")); }
+				 */
+
+				// STEP 6: Clean-up environment
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException se) {
+				// Handle errors for JDBC
+				se.printStackTrace();
+			} catch (Exception e) {
+				// Handle errors for Class.forName
+				e.printStackTrace();
+			} finally {
+				// finally block used to close resources
+				try {
+					if (stmt != null)
+						stmt.close();
+				} catch (SQLException se2) {
+				}// nothing we can do
+				try {
+					if (conn != null)
+						conn.close();
+				} catch (SQLException se) {
+					se.printStackTrace();
+				}// end finally try
+			}// end try
+
+			JtwigModelMap jtwigModelMap = new JtwigModelMap();
+			jtwigModelMap.add(map);
+
+			compiled.render(RenderContext.create(configuration.render(),
+					jtwigModelMap, outputStream));
+			out.println("twig output:" + outputStream.toString() + "");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			out.println("<br>exception: " + e.toString());
+		}
+
+		out.println("<br>getAuthType()- " + request.getAuthType());
+		out.println("<br>getDateHeader()- " + request.getDateHeader("temp"));
+		Enumeration<String> headers = request.getHeaderNames();
+		while (headers.hasMoreElements()) {
+			out.println("<br>" + headers.nextElement());
+		}
+		out.println("<br>getMethod()- " + request.getMethod());
+		out.println("<br>getPathInfo()- " + request.getPathInfo());
+		out.println("<br>getPathTranslated()- " + request.getPathTranslated());
+		out.println("<br>getQueryString()- " + request.getQueryString());
+		out.println("<br>getRemoteUser()- " + request.getRemoteUser());
+		out.println("<br>getRequestedSessionId()"
+				+ request.getRequestedSessionId());
+		out.println("<br>getRequestURI()- " + request.getRequestURI());
+		out.println("<br>getRequestURL()- " + request.getRequestURL());
+		out.println("<br>getServletPath()" + request.getServletPath());
+		out.println("<br>isRequestedSessionIdFromCookie()- "
+				+ request.isRequestedSessionIdFromCookie());
+		out.println("<br>isRequestedSessionIdFromUrl()- "
+				+ request.isRequestedSessionIdFromUrl());
+		out.println("<br>isRequestedSessionIdFromURL()- "
+				+ request.isRequestedSessionIdFromURL());
+		out.println("<br>isRequestedSessionIdValid()- "
+				+ request.isRequestedSessionIdValid());
+
+	}
+
+}
