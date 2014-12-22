91fa7506d4deb6e5f40605e98d83f6fc09ea2bb3
Günther Haslbeck <guentherhaslbeck@gmail.com>
2014-12-22 19:55:42 +0100

    may it work

---
+++ servlet2/src/main/java/com/ovmedia/servlet/Controller.java
@@ -0,0 +1,129 @@
+package main.java.com.ovmedia.servlet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lyncode.jtwig.JtwigModelMap;
+import com.lyncode.jtwig.JtwigTemplate;
+import com.lyncode.jtwig.configuration.JtwigConfiguration;
+import com.lyncode.jtwig.content.api.Renderable;
+import com.lyncode.jtwig.exception.CompileException;
+import com.lyncode.jtwig.exception.ParseException;
+import com.lyncode.jtwig.exception.RenderException;
+import com.lyncode.jtwig.render.RenderContext;
+
+public class Controller extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private HttpServletResponse _response;
+	
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		
+		doAll(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		doAll(request, response);
+	}
+
+	
+	protected void display( String templatename,Map<String, Object> map2) {
+		
+		try {
+			PrintWriter out = _response.getWriter();
+			_response.setContentType("text/html");
+			
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("_serverid", "1234");
+		
+			map.putAll(map2);
+			
+			JtwigConfiguration configuration = new JtwigConfiguration();
+
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			String defaultpath = "/var/lib/tomcat6/webapps/servletexample/WEB-INF/classes/";
+			Renderable compiled = new JtwigTemplate(
+					new File(defaultpath+templatename),configuration).compile();
+		
+
+			
+			
+			JtwigModelMap jtwigModelMap = new JtwigModelMap();
+			jtwigModelMap.add(map);
+
+			compiled.render(RenderContext.create(configuration.render(),jtwigModelMap, outputStream));
+			out.print(outputStream.toString());
+			
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CompileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RenderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+		
+	}
+
+	protected void doAll(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		this._response = response;
+/*
+		out.println("<br>getAuthType()- " + request.getAuthType());
+		out.println("<br>getDateHeader()- " + request.getDateHeader("temp"));
+		Enumeration<String> headers = request.getHeaderNames();
+		while (headers.hasMoreElements()) {
+			out.println("<br>" + headers.nextElement());
+		}
+		out.println("<br>getMethod()- " + request.getMethod());
+		out.println("<br>getPathInfo()- " + request.getPathInfo());
+		out.println("<br>getPathTranslated()- " + request.getPathTranslated());
+		out.println("<br>getQueryString()- " + request.getQueryString());
+		out.println("<br>getRemoteUser()- " + request.getRemoteUser());
+		out.println("<br>getRequestedSessionId()"
+				+ request.getRequestedSessionId());
+		out.println("<br>getRequestURI()- " + request.getRequestURI());
+		out.println("<br>getRequestURL()- " + request.getRequestURL());
+		out.println("<br>getServletPath()" + request.getServletPath());
+		out.println("<br>isRequestedSessionIdFromCookie()- "
+				+ request.isRequestedSessionIdFromCookie());
+		out.println("<br>isRequestedSessionIdFromUrl()- "
+				+ request.isRequestedSessionIdFromUrl());
+		out.println("<br>isRequestedSessionIdFromURL()- "
+				+ request.isRequestedSessionIdFromURL());
+		out.println("<br>isRequestedSessionIdValid()- "
+				+ request.isRequestedSessionIdValid());
+		*/
+
+	}
+
+}
---
+++ servlet2/src/main/java/com/ovmedia/servlet/Helper.java
@@ -0,0 +1,33 @@
+package main.java.com.ovmedia.servlet;
+
+import org.apache.log4j.DailyRollingFileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+public class Helper {
+
+	private  Logger _logger = Logger.getRootLogger();
+
+	public  void newLogger() {
+		try {
+			PatternLayout layout = new PatternLayout("%d{ISO8601} %-5p [%t] %c: %m%n");
+			DailyRollingFileAppender fileAppender = new DailyRollingFileAppender(layout, "logs/MeineLogDatei.log", "'.'yyyy-MM-dd_HH-mm");
+			_logger.addAppender(fileAppender);
+			_logger.setLevel(Level.ALL);
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+	}
+
+	public  void log(String message) {
+		try {
+			_logger.info(message);
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+	}
+}
